ce4da49e238490edeccac7a03a9b893424be1fd9
royljy <91171709+royljy@users.noreply.github.com>
2021-09-27 22:56:31 +0800

    Add files via upload

---
+++ spyder/assignment1_python.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 26 20:09:24 2021
+
+@author: roylee
+@title: st2195 Assignment 1
+"""
+
+for x in range(0,10):
+    print(x+1)
